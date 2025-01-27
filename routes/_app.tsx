aeeb81716e73df0f214ffd7ed632bf25a7bc5cf9
TanishqSingla <tanishqsingla00@gmail.com>
2025-01-27 12:12:23 +0530

    feat: update data modeling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,15 @@
 import { type PageProps } from "$fresh/server.ts";
+
 export default function App({ Component }: PageProps) {
 	return (
-		<html class="bg-surface">
+		<html>
 			<head>
 				<meta charset="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>ledger</title>
 				<link rel="stylesheet" href="/styles.css" />
 			</head>
-			<body>
+			<body class='bg-background'>
 				<Component />
 			</body>
 		</html>
--- types.ts
+++ types.ts
@@ -1,9 +1,35 @@
-export type User = {
-	email_id: string;
-};
+type id = string;
 
 export type Account = {
-	id: string;
-	Name: string;
-	Accounts: string[];
+	pk: `ACC#${id}`;
+	sk: `ACC#${id}`;
+	AccountId: id;
+	IsUserOwned: boolean;
+	BankDetails: {
+		AccountName: string;
+		AccountNumber?: string;
+		IFSC?: string;
+	};
+};
+
+export type Payment = {
+	pk: `PM#${id}`;
+	sk: string;
+	AccountId: string;
+	Amount: string;
+	InvoiceDate: string;
+	BeneficiaryId: string;
+};
+
+export type Record = {
+	pk: `REC#${id}`;
+	sk: `REC#${id}`;
+	AccountId: string;
+	Amount: string;
+	BeneficiaryId: string;
+	InvoiceDate: string;
+	PaymentMethod: "CHQ" | "NEFT" | "RTGS" | "UPI";
+	PaymentDate: string;
+	Ref: string;
+	TransactionType: "credit" | "debit";
 };
