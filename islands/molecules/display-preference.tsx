079c09e296f8532dd7036b6169fb9bbd166421a4
TanishqSingla <tanishqsingla00@gmail.com>
2025-04-01 04:01:56 +0530

    fix: localstorage

--- islands/molecules/display-preference.tsx
+++ islands/molecules/display-preference.tsx
@@ -1,4 +1,4 @@
-import { effect, signal } from "@preact/signals";
+import { computed, effect, signal } from "@preact/signals";
 import { GridIcon, ListIcon } from "../../components/icons/index.tsx";
 
 export const DISPLAY_TYPE = {
@@ -6,15 +6,20 @@ export const DISPLAY_TYPE = {
 	GRID: "GRID",
 } as const;
 
-export const displayTypeSignal = signal<string>(DISPLAY_TYPE.GRID);
+export const displayTypeSignal = signal<string | null>();
 
-if (localStorage.getItem('displayType')) {
-	displayTypeSignal.value = localStorage.getItem('displayType')!;
-}
+export const displayTypeComputed = computed(() => {
+	if (!displayTypeSignal.value) {
+		return localStorage.getItem('displayType') || DISPLAY_TYPE.GRID;
+	}
+
+	return displayTypeSignal.value;
+})
 
 effect(() => {
-	localStorage.setItem('displayType', displayTypeSignal.value);
-})
+	if (displayTypeComputed.value)
+		localStorage.setItem('displayType', displayTypeComputed.value)
+});
 
 export default function DisplayPreference() {
 	const handleClick = (event: any) => {
@@ -30,7 +35,7 @@ export default function DisplayPreference() {
 				value={DISPLAY_TYPE.LIST}
 				className={"px-4 py-2 text-onSurface hover:bg-onSurface data-[selected=true]:bg-secondaryContainer hover:bg-opacity-[0.08]"}
 				aria-selected={false}
-				data-selected={displayTypeSignal.value === DISPLAY_TYPE.LIST}
+				data-selected={displayTypeComputed.value === DISPLAY_TYPE.LIST}
 				onClick={handleClick}
 			>
 				<ListIcon />
@@ -40,7 +45,7 @@ export default function DisplayPreference() {
 				value={DISPLAY_TYPE.GRID}
 				className={"py-2 px-4 text-onSurface hover:bg-onSurface data-[selected=true]:bg-secondaryContainer hover:bg-opacity-[0.08]"}
 				aria-selected={false}
-				data-selected={displayTypeSignal.value === DISPLAY_TYPE.GRID}
+				data-selected={displayTypeComputed.value === DISPLAY_TYPE.GRID}
 				onClick={handleClick}
 			>
 				<GridIcon />
