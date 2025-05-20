eafb6b2217ebdeb1e81d0a3e0219ca0bd176529b
TanishqSingla <tanishqsingla00@gmail.com>
2025-05-20 23:13:42 +0530

    feat: add page for showing archive bills

---
+++ routes/dashboard/archive-bills/index.tsx
@@ -0,0 +1,61 @@
+import { GetArchiveBills } from "@db/ArchiveBills.ts";
+import { NoData } from "@components/icons/index.tsx";
+
+const cols = [
+	{ name: "Bill Number" },
+	{ name: "Bill Date" },
+	{ name: "Due Date" },
+	{ name: "Amount" },
+	{ name: "Archived At" },
+	{ name: "Actions" },
+];
+
+export default async function ArchiveBills() {
+	const archives = await GetArchiveBills();
+
+	return (
+		<div class="p-4">
+			<h1 class="text-display-medium">Archive bills</h1>
+
+			<div class="rounded-xl overflow-hidden relative mt-8 border">
+				<table class="table-auto w-full border-collapse">
+					<thead>
+						<tr>
+							{cols.map((col) => (
+								<th
+									key={col.name}
+									class="px-4 py-2 bg-surfaceContainer text-left"
+								>
+									{col.name}
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody>
+						{!archives.length && (
+							<tr className="min-h-60">
+								<td colspan={7}>
+									<div className="flex flex-col items-center justify-center my-8">
+										<NoData width={128} height={128} />
+										<p className="text-center">No Data</p>
+									</div>
+								</td>
+							</tr>
+						)}
+						{archives.map((archive) => (
+							<tr key={archive.bill_id}>
+								<td class="px-4 py-2 text-label-large">{archive.bill_id}</td>
+								<td class="px-4 py-2 text-label-large">{archive.created_at}</td>
+								<td class="px-4 py-2 text-label-large">{archive.created_at}</td>
+								<td class="px-4 py-2 text-label-large">{archive.amount}</td>
+								<td class="px-4 py-2 text-label-large">
+									{archive.archived_at}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+}
