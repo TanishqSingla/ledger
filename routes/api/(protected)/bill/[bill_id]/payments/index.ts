261850049134576be7f53fb2a4a35fa9cf48d486
TanishqSingla <tanishqsingla00@gmail.com>
2025-06-04 01:48:16 +0530

    feat: add api to delete payments

--- routes/api/(protected)/bill/[bill_id]/payments/index.ts
+++ routes/api/(protected)/bill/[bill_id]/payments/index.ts
@@ -1,7 +1,8 @@
 import { Handlers } from "$fresh/server.ts";
-import { PostBillPayment } from "@db/Bills.ts";
+import { DeletePayment, PostBillPayment } from "@db/Bills.ts";
+import { delete_file } from "@queries/s3.ts";
 
-export const handler: Handlers = {
+export const handler: Handlers<{}, { email_id: string }> = {
 	async POST(req, ctx) {
 		const billId = ctx.params.bill_id;
 		const user = ctx.state.email_id as string;
@@ -15,6 +16,35 @@ export const handler: Handlers = {
 			console.error(err);
 
 			return new Response(JSON.stringify({ error: err }), { status: 500 });
+		}
+	},
+	async DELETE(req, ctx) {
+		const billId = ctx.params.bill_id;
+		const user = ctx.state.email_id;
+		const payload = await req.json();
+
+		try {
+			const resp = {};
+
+			if (payload?.file) {
+				const _resp = await delete_file(payload.file);
+				Object.assign(resp, _resp);
+			}
+
+			const _resp = DeletePayment({
+				bill_id: billId,
+				file: payload?.file,
+				reference_number: payload?.reference_number,
+				user,
+			});
+
+			Object.assign(resp, _resp);
+
+			return Response.json(resp);
+		} catch (err) {
+			console.log(err);
+
+			return Response.json({ err }, { status: 500 });
 		}
 	},
 };
