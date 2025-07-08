6e87575fbbb5f500624a8c819b26f82a40db572a
TanishqSingla <tanishqsingla00@gmail.com>
2025-07-08 17:27:14 +0530

    refactor: lint warnings

--- routes/dashboard/accounts/index.tsx
+++ routes/dashboard/accounts/index.tsx
@@ -1,8 +1,8 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Button } from "ketu";
-import { NoData } from "../../../components/icons/index.tsx";
-import { AccountDocument, GetAllAccounts } from "../../../db/Accounts.ts";
-import { buttonVariants } from "../../../components/Button.tsx";
+import { NoData } from "@components/icons/index.tsx";
+import { AccountDocument, GetAllAccounts } from "@db/Accounts.ts";
+import { buttonVariants } from "@components/Button.tsx";
 
 type Data = {
 	accounts: AccountDocument[];
@@ -35,15 +35,15 @@ export default function Accounts({ data }: PageProps<Data>) {
 			{data.accounts.length
 				? (
 					<div class="rounded-xl overflow-hidden relative mt-8 border">
-						{data.accounts.map((account) => <p>{account.account_name}</p>)}
+						{data.accounts.map((account) => (
+							<p key={account.account_id}>{account.account_name}</p>
+						))}
 					</div>
 				)
 				: (
-					<div
-						className={"flex gap-4 flex-col items-center justify-center rounded-xl overflow-hidden relative mt-8 border min-h-60"}
-					>
+					<div className="flex gap-4 flex-col items-center justify-center rounded-xl overflow-hidden relative mt-8 border min-h-60">
 						<NoData width={128} height={128} />
-						<p className={"text-center"}>No Data</p>
+						<p className="text-center">No Data</p>
 					</div>
 				)}
 		</div>
