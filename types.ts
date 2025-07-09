567f4f79988094a466fb5e14829a8fb128d6bb95
TanishqSingla <tanishqsingla00@gmail.com>
2025-07-09 20:02:28 +0530

    chore: cleanup

--- types.ts
+++ types.ts
@@ -19,4 +19,44 @@ export type Vendor = {
 
 export type VendorDocument = Vendor & MongoDocument;
 
+export type Company = {
+	company_id: string;
+	company_name: string;
+	company_accounts: {
+		id: string;
+		bank_name?: string;
+		branch_name?: string;
+		company_number: string;
+		ifsc: string;
+	}[];
+};
 
+export type CompanyDocument = Company & MongoDocument;
+
+export type BankInfo = {
+	"MICR": string;
+	"BRANCH": string;
+	"ADDRESS": string;
+	"STATE": string;
+	"CONTACT": string;
+	"UPI": boolean;
+	"RTGS": boolean;
+	"CITY": string;
+	"CENTRE": string;
+	"DISTRICT": string;
+	"NEFT": boolean;
+	"IMPS": boolean;
+	"SWIFT": string;
+	"ISO3166": string;
+	"BANK": string;
+	"BANKCODE": string;
+	"IFSC": string;
+};
+
+export type Payment = {
+	reference_number: string;
+	bill_to: string;
+	paid_from: Company;
+	payment_time: Date;
+};
+export type PaymentDocument = Payment & MongoDocument;
