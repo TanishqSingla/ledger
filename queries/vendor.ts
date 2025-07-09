4c4004429e326b6c1643d59cf0c981d97f69587b
TanishqSingla <tanishqsingla00@gmail.com>
2025-07-09 20:07:48 +0530

    chore: cleanup

--- queries/vendor.ts
+++ queries/vendor.ts
@@ -1,29 +1,9 @@
-import { Vendor, VendorDocument } from "../db/Vendors.ts";
+import { BankInfo, Vendor, VendorDocument } from "@/types.ts";
 
 export type PutVendorPayload = {
 	vendor_name: string;
 	email?: string;
 	phone?: string;
-};
-
-export type BankInfo = {
-	"MICR": string;
-	"BRANCH": string;
-	"ADDRESS": string;
-	"STATE": string;
-	"CONTACT": string;
-	"UPI": boolean;
-	"RTGS": boolean;
-	"CITY": string;
-	"CENTRE": string;
-	"DISTRICT": string;
-	"NEFT": boolean;
-	"IMPS": boolean;
-	"SWIFT": string;
-	"ISO3166": string;
-	"BANK": string;
-	"BANKCODE": string;
-	"IFSC": string;
 };
 
 export const putVendor = async (body: PutVendorPayload) => {
